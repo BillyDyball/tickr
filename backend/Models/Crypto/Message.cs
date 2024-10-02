fae3c9b552e1345a71ad8cc7b2443aeb770e2cf9
BillyDyball <Billyjdyball@gmail.com>
2024-10-02 10:47:06 +0100

    Created CryptoMessage

---
+++ backend/Models/Crypto/Message.cs
@@ -0,0 +1,17 @@
+namespace TickrApi.Models;
+
+public class CryptoMessage
+{
+    public string D { get; set; }
+    public string P { get; set; }
+    public string Ch { get; set; }
+    public string F { get; set; }
+    public string Aggr { get; set; }
+    public string S { get; set; }
+    public long T { get; set; }
+    public double O { get; set; }
+    public double H { get; set; }
+    public double L { get; set; }
+    public double C { get; set; }
+    public double V { get; set; }
+}
